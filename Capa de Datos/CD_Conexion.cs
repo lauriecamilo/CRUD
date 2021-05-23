8221e90b21702f5b2223369041fe7f4350853541
Laurie Camilo <lauriecarol0327@gmail.com>
2021-05-23 19:08:20 -0400

    CRUD con NCapas

---
+++ Capa de Datos/CD_Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Capa_de_Datos
+{
+    public class CD_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=USUARIO;DataBase= Practica;Integrated Security=true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
